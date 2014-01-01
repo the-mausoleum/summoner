6f0b47478f87a8ad276284d63fe795819a1c289c
maxdeviant <mad.mod.max@gmail.com>
2014-01-01 16:12:52 -0500

    Finished API calls & added API versioning.

--- api.js
+++ api.js
@@ -1,50 +1,78 @@
 App.factory('API', function($http) {
 	var region = "na";
-	var version = "v1.1"
-	var key = "?api_key=4e5378a0-1af3-41e2-a571-3723af9ab9e8"
-	var baseURL = "https://prod.api.pvp.net/api/lol/" + region + "/" + version;
+	var versions = Object.freeze({
+		"champion": "v1.1",
+		"game": "v1.2",
+		"league": "v2.2",
+		"stats": "v1.2",
+		"summoner": "v1.2",
+		"team": "v2.2"
+	});
+	var key = "4e5378a0-1af3-41e2-a571-3723af9ab9e8"
+	var baseURL = "https://prod.api.pvp.net/api/lol/" + region + "/";
 
 	function GET(url) {
 		// Return AJAX promise token
-		return $http.get(baseURL + url + key);
+		return $http.get(baseURL + url + "?api_key=" + key);
 	}
 
 	var API = function() {};
 
 	// Retrieve all champions
 	API.getChampions = function() {
-		return GET("/champion");
+		return GET(versions["champion"] + "/champion");
 	}
 
 	// Get recent games by summoner ID
 	API.getGames = function(id) {
-		return GET("/game/by-summoner/" + id + "/recent");
+		return GET(versions["games"] + "/game/by-summoner/" + id + "/recent");
+	}
+
+	// Retrieves league data for summoner, including leagues for all of summoner's teams
+	API.getLeagues = function(id) {
+		return GET(versions["league"] + "/league/by-summoner/" + id);
+	}
+
+	// Get player stats summaries by summoner ID. One summary is returned per queue type
+	API.getStatsSummary = function(id) {
+		return GET(versions["stats"] + "stats/by-summoner/" + id + "/summary");
+	}
+
+	// Get ranked stats by summoner ID. Includes statistics for Twisted Treeline and Summoner's Rift
+	API.getRankedStats = function(id) {
+		return GET(versions["stats"] + "stats/by-summoner/" + id + "/ranked");
 	}
 
 	API.Summoner = {};
 
-	// Get summoner by name
-	API.Summoner.getByName = function(name) {
-		return GET("/summoner/by-name/" + name);
-	}
-
-	// Get summoner by summoner ID
-	API.Summoner.getByID = function(id) {
-		return GET("/summoner/" + id);
-	}
-
-	API.Summoner.getListByID = function(id_list) {
-		return GET("/summoner/" + id_list + "/name")
-	}
-
 	// Get mastery pages by summoner ID
 	API.Summoner.getMasteries = function(id) {
-		return GET("/summoner/" + id + "/masteries");
+		return GET(versions["summoners"] + "/summoner/" + id + "/masteries");
 	}
 
 	// Get rune pages by summoner ID
 	API.Summoner.getRunes = function(id) {
-		return GET("/summoner/" + id + "/runes");
+		return GET(versions["summoners"] + "/summoner/" + id + "/runes");
+	}
+
+	// Get summoner by name
+	API.Summoner.getByName = function(name) {
+		return GET(versions["summoner"] + "/summoner/by-name/" + name);
+	}
+
+	// Get summoner by summoner ID
+	API.Summoner.getByID = function(id) {
+		return GET(versions["summoners"] + "/summoner/" + id);
+	}
+
+	// Get list of summoners by summoner IDs
+	API.Summoner.getListByID = function(id_list) {
+		return GET(versions["summoners"] + "/summoner/" + id_list + "/name")
+	}
+
+	// Retrieves teams for given summoner ID
+	API.getTeams = function(id) {
+		return GET(versions["team"] + "/team/by-summoner/" + id);
 	}
 
 	return API;
