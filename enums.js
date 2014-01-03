8fb8f87fba993a30a20b2ce437ce8ce6097ff0b6
maxdeviant <mad.mod.max@gmail.com>
2014-01-02 21:06:27 -0500

    Additional summoner icons.
    I may be doing this for a while.

--- enums.js
+++ enums.js
@@ -32,6 +32,10 @@ angular.module('App').factory('Enums', function() {
 			"Icon": "assets/summoner_icons/ProfileIcon08.jpg"
 		},
 		{
+			"ID": 11,
+			"Icon": "assets/summoner_icons/ProfileIcon12.jpg"
+		},
+		{
 			"ID": 21,
 			"Icon": "assets/summoner_icons/ProfileIcon22.jpg"
 		},
@@ -40,17 +44,37 @@ angular.module('App').factory('Enums', function() {
 			"Icon": "assets/summoner_icons/ProfileIcon62.jpg"
 		},
 		{
+			"ID": 555,
+			"Icon": "assets/summoner_icons/ProfileIcon73.jpg"
+		},
+		{
 			"ID": 579,
 			"Icon": "assets/summoner_icons/ProfileIcon_Tome.png"
+		},
+		{
+			"ID": 585,
+			"Icon": "assets/summoner_icons/image_104.jpg"
+		},
+		{
+			"ID": 586,
+			"Icon": "assets/summoner_icons/image_108.jpg"
 		},
 		{
 			"ID": 588,
 			"Icon": "assets/summoner_icons/ProfileIcon_Poro.png"
 		},
 		{
+			"ID": 589,
+			"Icon": "assets/summoner_icons/ProfileIcon_BadSanta.png"
+		},
+		{
 			"ID": 591,
 			"Icon": "assets/summoner_icons/ProfileIcon_Teemo.png"
-		}
+		},
+		{
+			"ID": 592,
+			"Icon": "assets/summoner_icons/ProfileIcon_Santa.png"
+		},
 		// {
 		// 	"ID": ,
 		// 	"Icon": "assets/summoner_icons/"
